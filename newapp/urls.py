4a11cce7fe906ef1898106bbe75c8538d844c8ea
Adhil V T <adhilvt369@gmail.com>
2026-05-22 12:15:02 +0530

    third commit

--- newapp/urls.py
+++ newapp/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('')
+    path('', views.task_list),
+    path('create/', views.task_create),
+    path('update/<int:pk>/', views.task_update),
+    path('partial-update/<int:pk>/', views.task_partial_update),
+    path('delete/<int:pk>/', views.task_delete),
 ]
 
--- newapp/views.py
+++ newapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Task
+from .serialiser import TaskSerializer
 
 @api_view(['GET'])
 def task_list(request):
@@ -16,5 +17,28 @@ def task_create(request):
         serializer.save()
     return Response(serializer.data)
 
+@api_view(['PUT'])
+def task_update(request, pk):
+    task = Task.objects.get(id=pk)
+    serializer = TaskSerializer(instance=task, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+    return Response("Task updated successfully")
+    
+@api_view(['PATCH'])
+def task_partial_update(request, pk):
+    task = Task.objects.get(id=pk)
+    serializer = TaskSerializer(instance=task, data=request.data, partial=True)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+    return Response("Task updated successfully")
+
+@api_view(['DELETE'])
+def task_delete(request, pk):
+    task = Task.objects.get(id=pk)
+    task.delete()
+    return Response("Task deleted successfully")
 
 # Create your views here.
