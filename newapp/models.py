9b6aa3f9493889200aee3cff1d15c00b24bfa61b
Adhil V T <adhilvt369@gmail.com>
2026-05-22 11:59:08 +0530

    second commit

--- newapp/models.py
+++ newapp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
+class Task(models.Model):
+    task = models.CharField(max_length=200)
+    date = models.DateField()
+
 # Create your models here.
--- newapp/views.py
+++ newapp/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Task
+
+@api_view(['GET'])
+def task_list(request):
+    tasks = Task.objects.all()
+    serializer = TaskSerializer(tasks, many=True)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def task_create(request):
+    serializer = TaskSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
 
 # Create your views here.
